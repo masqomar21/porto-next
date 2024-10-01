7f3931a4f3e6db2c78d3dc721327279ac8429029
masqomar21 <mqomarudin0480@gmail.com>
2024-10-01 10:34:32 +0700

    feat() - fix porto

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,9 +26,9 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Mail From <onboarding@resend.dev>",
       to: "mqomarudin0480@gmail.com",
-      subject: "Message from contact form",
+      subject: "Message from your portfolio site",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
